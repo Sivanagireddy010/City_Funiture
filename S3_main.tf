d4bad5dd0dbf4d809195f6036c34ca843e63605e
Sivanagireddy010 <146823008+Sivanagireddy010@users.noreply.github.com>
2026-04-30 14:01:01 +0530

    Create S3_main.tf

---
+++ S3_main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-gha-terraform-bucket-123456"
+
+  tags = {
+    Name        = "DemoBucket"
+    Environment = "Dev"
+  }
+}
